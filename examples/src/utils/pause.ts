71b6b2f7592fc19208aa260fc66e653247cbb038
aquapi <aquaplmc@gmail.com>
2025-04-06 03:31:08 +0700

    Fix

--- examples/src/utils/pause.ts
+++ examples/src/utils/pause.ts
@@ -3,20 +3,23 @@ import * as cio from 'ciorent';
 // Expensive sync task
 const task1 = async () => {
   let x = 0;
-  for (let i = 0; i < (Math.random() + 15) * 1e7; i++) {
-    // Frequent pausing
-    if (i % 2e6 === 0)
-      await cio.pause;
 
+  // Pause to let task2 to run
+  await cio.pause;
+
+  for (let i = 0; i < (Math.random() + 15) * 1e6; i++)
     x += Math.random() * 32 + i * Math.round(Math.random() * 16);
-  }
+
   console.log('Finish task 1:', x);
 };
 
 // Short async task
 const task2 = async () => {
   console.log('Fetch start', performance.now().toFixed(2) + 'ms');
+
+  // This will pause task2 to let task1 to continue running
   const txt = await fetch('http://example.com');
+
   console.log('Fetch status', txt.status);
 };
 
