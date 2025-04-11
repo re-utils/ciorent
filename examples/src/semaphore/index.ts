9828ba19232e2aac3ca03fccc3381a780edced03
aquapi <aquaplmc@gmail.com>
2025-04-11 11:08:48 +0700

    Fix semaphore example

--- examples/src/semaphore/index.ts
+++ examples/src/semaphore/index.ts
@@ -1,19 +1,24 @@
 import * as semaphore from 'ciorent/semaphore';
 import * as cio from 'ciorent';
 
-const task = semaphore.wrap(
-  async (task: number) => {
-    console.log('Task', task, 'started');
-
-    for (let i = 1; i <= 5; i++)
-      await cio.pause;
-
-    console.log('Task', task, 'end');
-  }
-);
-
 // Only allow 2 task to run concurrently
 const sem = semaphore.init(2);
 
+const task = async (id: number) => {
+  // Acquire the semaphore for this task
+  // Or wait for the semaphore to be available
+  await semaphore.pause(sem);
+
+  console.log('Task', id, 'started');
+
+  // Let the main thread schedules other tasks
+  for (let i = 1; i <= 5; i++) await cio.pause;
+
+  console.log('Task', id, 'end');
+
+  // Release the semaphore
+  semaphore.signal(sem);
+}
+
 // Try to run 5 tasks concurrently
-cio.concurrent(5, (sem, id) => task(sem, id), sem);
+cio.concurrent(5, task);
