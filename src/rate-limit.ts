2b954f77a7db3436335cbcb0a83da7dd2b8a19b4
aquapi <aquaplmc@gmail.com>
2025-05-07 00:31:29 +0700

    Rate limit

---
+++ src/rate-limit.ts
@@ -0,0 +1,68 @@
+export type Limiter = (calls: number, ms: number) => () => boolean;
+
+/**
+ * Fixed window strategy
+ * @param limit
+ * @param ms
+ */
+export const fixed: Limiter = (limit, ms) => {
+  let cur = limit;
+  const unlock = () => {
+    cur = limit;
+  }
+
+  return () => {
+    if (cur === 0) return false;
+
+    if (cur === limit)
+      setTimeout(unlock, ms);
+    cur--;
+    return true;
+  }
+}
+
+/**
+ * Sliding window strategy
+ * @param limit
+ * @param ms
+ */
+export const sliding: Limiter = (limit, ms) => {
+  let cur = limit;
+  const unlock = () => {
+    cur++;
+  }
+
+  return () => {
+    if (cur === 0) return false;
+
+    cur--;
+    setTimeout(unlock, ms);
+    return true;
+  }
+}
+
+/**
+ * Token bucket strategy
+ * @param limit
+ * @param ms
+ */
+export const bucket: Limiter = (limit, ms) => {
+  let cur = limit;
+
+  ms = ms / limit;
+  const unlock = () => {
+    cur++;
+    if (cur < limit)
+      setTimeout(unlock, ms);
+  }
+
+  return () => {
+    if (cur === 0) return false;
+
+    if (cur === limit)
+      setTimeout(unlock, ms);
+
+    cur--;
+    return true;
+  }
+}
