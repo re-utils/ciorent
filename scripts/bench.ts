ea5ae75b7d78440e346e1ae4e499b31720ee6ef3
aquapi <aquaplmc@gmail.com>
2025-08-05 19:15:45 +0700

    Update bench

--- scripts/bench.ts
+++ scripts/bench.ts
@@ -4,7 +4,7 @@ import { BENCH, cd, exec } from './utils.ts';
 
 const CMD: Dict<string> = {
   bun: 'bun run',
-  node: 'bun jiti --expose-gc --allow-natives-syntax',
+  node: 'node --import jiti/register --expose-gc --allow-natives-syntax',
   deno: 'deno run --allow-all --v8-flags=--expose-gc,--allow-natives-syntax',
 };
 
@@ -23,7 +23,7 @@ const args = parseArgs({
 }).values;
 
 const EXE = CMD[args.runtime];
-if (EXE == null) throw new Error('Unrecognized runtime: ' + EXE);
+if (EXE == null) throw new Error('Unrecognized runtime: ' + args.runtime);
 
 cd(BENCH);
 
