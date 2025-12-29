417ad90e6229ed3839392a2684a0e0cbc7d84074
aquapi <aquaplmc@gmail.com>
2025-12-29 23:23:05 +0700

    remove all micro utils

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import {
-  loadedReject,
-  loadedResolve,
-  loadResolve,
-  loadResolvers,
-} from './utils.js';
-
 /**
  * Continue the execution on next event loop cycle.
  *
@@ -12,30 +5,6 @@ import {
  * blocking the main thread and let other asynchronous task to run.
  */
 export const nextTick: Promise<void> = Promise.resolve();
-
-/**
- * Check whether a value is awaitable.
- * @param p
- * @returns
- */
-export const isThenable = <T>(p: unknown): p is PromiseLike<T> =>
-  p !== null &&
-  typeof p === 'object' &&
-  !Array.isArray(p) &&
-  // @ts-ignore
-  typeof p.then === 'function';
-
-/**
- * Timeout a promise without swallowing errors.
- * @param p
- * @param ms
- */
-export const timeout = <T>(p: Promise<T>, ms: number): Promise<T | void> => {
-  const promise = new Promise<void>(loadResolvers);
-  setTimeout(loadedResolve, ms);
-  p.then(loadedResolve, loadedReject);
-  return promise;
-};
 
 export * as mutex from './mutex.js';
 export * as limit from './rate-limit.js';
